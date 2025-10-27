3329624fdaf0c098b7c50ebf9776b46896e5407a
Jonathan Lacroix <2397505@cmontmorency.qc.ca>
2025-10-27 13:18:30 +0000

    [AJOUT] Getters et Setters de Book

--- book.cpp
+++ book.cpp
@@ -7,3 +7,17 @@ Book::Book() : title(""), author(""), isbn(""), isAvailable(true), borrowerName(
 
 Book::Book(const string& title, const string& author, const string& isbn)
     : title(title), author(author), isbn(isbn), isAvailable(true), borrowerName("") {}
+
+// Get
+string Book::getTitle() const {return title;}
+string Book::getAuthor() const {return author;}
+string Book::getISBN() const {return isbn;}
+bool Book::getAvailability() const {return isAvailable;}
+string Book::getBorrowerName() const {return borrowerName;}
+
+// Set
+void Book::setTitle(const string& t) {title=t;}
+void Book::setAuthor(const string& a) {author=a;}
+void Book::setISBN(const string& i) {title=i;}
+void Book::setAvailability(bool available) {isAvailable = available;}
+void Book::setBorrowerName(const string& name) {borrowerName = name;}
