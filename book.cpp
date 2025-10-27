f3fa538019eb0b0d1d3ccce708f4fa2a043ec716
Jonathan Lacroix <2397505@cmontmorency.qc.ca>
2025-10-27 13:19:35 +0000

    [AJOUT] Méthodes principales : emprunt, retour, affichage et sauvegarde

--- book.cpp
+++ book.cpp
@@ -21,3 +21,45 @@ void Book::setAuthor(const string& a) {author=a;}
 void Book::setISBN(const string& i) {title=i;}
 void Book::setAvailability(bool available) {isAvailable = available;}
 void Book::setBorrowerName(const string& name) {borrowerName = name;}
+
+// Méthodes
+void Book::checkOut(const string& borrower) {
+    if (isAvailable) {
+        isAvailable = false;
+        borrowerName = borrower;
+    }
+}
+
+void Book::returnBook() {
+    isAvailable = true;
+    borrowerName = "";
+}
+
+void Book::toString() const {
+    cout << title << " — " << author << " (" << isbn << ")";
+    if (isAvailable)
+        cout << " [Disponible]";
+    else
+        cout << " [Emprunté par " << borrowerName << "]";
+    cout << endl;
+}
+
+string Book::toFileFormat() const {
+    string line = title + ";" + author + ";" + isbn + ";";
+    line += (isAvailable ? "1" : "0");
+    line += ";" + borrowerName;
+    return line;
+}
+
+void Book::fromFileFormat(const string& line) {
+    int pos1 = line.find(';');
+    int pos2 = line.find(';', pos1 + 1);
+    int pos3 = line.find(';', pos2 + 1);
+    int pos4 = line.find(';', pos3 + 1);
+    title = line.substr(0, pos1);
+    author = line.substr(pos1 + 1, pos2 - pos1 - 1);
+    isbn = line.substr(pos2 + 1, pos3 - pos2 - 1);
+    string available = line.substr(pos3 + 1, pos4 - pos3 - 1);
+    borrowerName = line.substr(pos4 + 1);
+    isAvailable = (available == "1");
+}
